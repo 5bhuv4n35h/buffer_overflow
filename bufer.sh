3c4b8560d285ca1e044f506317ba66aae24d244b
5bhuv4n35h <10207922+5bhuv4n35h@users.noreply.github.com>
2020-05-11 12:17:47 -0400

    Create bufer.sh
    buffer overflow create  unique string and identify the eip using metasploit framework

---
+++ bufer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+##### (Cosmetic) Colour output
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings/Information
+BLUE="\033[01;34m"     # Heading
+BOLD="\033[01;01m"     # Highlight
+RESET="\033[00m"       # Normal
+echo -e "${GREEN}"
+##################################installation starts ######################################
+options=("create unique pattern for buffer overflow " "locate patter offset for bufer overflow"   "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+    
+    "create unique pattern for buffer overflow ")              
+        echo -e "${GREEN} "
+                  /usr/bin/msf-pattern_create -h
+         read -p "enter the  length of buffer" lent
+         /usr/bin/msf-pattern_create -l  $lent
+
+             ;;
+    "locate patter offset for bufer overflow")
+           /usr/bin/msf-pattern_offset -h
+           echo -e "\n remember the length of buffer from previous step && copy the stringfrom eip"
+           read -p "enter the length of buffer " lenb
+           read -p "enter the eip string " eipb
+           /usr/bin/msf-pattern_offset -l $lenb -q $eipb
+         ;;    
+    "Quit")
+       break
+       ;;
+    *) 
+     echo invalid option
+     ;;
+    esac
+done
